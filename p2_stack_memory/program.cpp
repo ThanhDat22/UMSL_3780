4713625a8974c95208bbc256fe9323a5e3d7b91e
ThanhDat22 <dat.nguyen2284@gmail.com>
2025-03-09 13:30:04 -0500

    p3 1st on 2025-03-09

---
+++ p2_stack_memory/program.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// Function declaration
+void func_1(int);
+void func_2(int);
+void func_3(int);
+
+// Main function
+int main() {
+    int num = 10;
+    func_1(num);
+    return 0;
+}
+
+// Function definition
+
+int func_1(int a) {
+    int x = a;
+    return func_2(x);
+}
+
+int func_2(int b) {
+    int y = b + 1;
+    return func_3(y);
+}
+
+int func_3(int c) {
+    int z = c + 1;
+    cout << "The final value is: " << z << endl;
+    return z;
+}
